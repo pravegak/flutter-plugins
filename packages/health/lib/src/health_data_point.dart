947f34795618da4b6d95095ba9be83cb1d03c624
Pulkit Goyal <7895pulkit@gmail.com>
2021-06-28 04:32:58 +0530

    Revert "hack around to have an empty constructor"
    This reverts commit f8ec1fb34f33a0a0987cab353b6855251ae5fab8.

--- packages/health/lib/src/health_data_point.dart
+++ packages/health/lib/src/health_data_point.dart
@@ -129,20 +129,18 @@ class HealthDataPoint extends AbstractDataPoint {
 }
 
 class WorkoutDataPoint extends AbstractDataPoint {
-  late HealthDataType _type;
-  late DateTime _dateFrom;
-  late DateTime _dateTo;
-  late PlatformType _platform;
-  late String _deviceId;
-  late String _sourceId;
-  late String _sourceName;
-  late String _deviceModel;
-  late String _activityType;
-  late double _totalDistance;
-  late double _totalEnergyBurned;
-  late double _duration;
-
-  WorkoutDataPoint();
+  HealthDataType _type;
+  DateTime _dateFrom;
+  DateTime _dateTo;
+  PlatformType _platform;
+  String _deviceId;
+  String _sourceId;
+  String _sourceName;
+  String _deviceModel;
+  String _activityType;
+  Double _totalDistance;
+  Double _totalEnergyBurned;
+  Double _duration;
 
   WorkoutDataPoint._(
     this._type,
@@ -204,11 +202,11 @@ class WorkoutDataPoint extends AbstractDataPoint {
 
   String get activityType => _activityType;
 
-  double get totalDistance => _totalDistance;
+  Double get totalDistance => _totalDistance;
 
-  double get totalEnergyBurned => _totalEnergyBurned;
+  Double get totalEnergyBurned => _totalEnergyBurned;
 
-  double get duration => _duration;
+  Double get duration => _duration;
 
   /// An equals (==) operator for comparing two data points
   /// This makes it possible to remove duplicate data points.
--- packages/health/lib/src/health_factory.dart
+++ packages/health/lib/src/health_factory.dart
@@ -119,10 +119,10 @@ class HealthFactory {
             e["device_model"] == null ? "manually stored" : e["device_model"];
 
         if (e['sample_type'] == 'workout') {
-          final double duration = e['duration'];
+          final Double duration = e['duration'];
           final String activityType = e['workout_activity_type'];
-          final double distance = e['total_distance'];
-          final double energyBurned = e['total_energy_burned'];
+          final Double distance = e['total_distance'];
+          final Double energyBurned = e['total_energy_burned'];
           return WorkoutDataPoint._(
             dataType,
             from,
@@ -138,7 +138,6 @@ class HealthFactory {
             duration,
           );
         }
-        return WorkoutDataPoint();
         // final num value = e['value'];
         // final unit = _dataTypeToUnit[dataType]!;
         // return HealthDataPoint._(
